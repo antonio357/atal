1e4b79a01d5333fa214f10d24619c3df747293af
antonio357 <carlosalbuquerque359@gmail.com>
2021-10-02 18:46:20 -0300

    identação corrigida, codigo funcionando como o do professor

--- Henrique/dynamic programming/cut_rod_by_henrique/cut_rod_memoizado.py
+++ Henrique/dynamic programming/cut_rod_by_henrique/cut_rod_memoizado.py
@@ -47,26 +47,26 @@ INT_MIN = -32767
 # Returns the best obtainable price for a rod of length n and
 # price[] as prices of different pieces
 def cutRod(price, n):
-	# print('cutRod')
-	val = [0 for x in range(n+1)]
-	val[0] = 0
+    # print('cutRod')
+    val = [0 for x in range(n+1)]
+    val[0] = 0
 
-	# Build the table val[] in bottom up manner and return
-	# the last entry from the table
-	# print('val =', val)
-	for i in range(1, n+1):
-		# print('i =', i)
-		max_val = INT_MIN
-		for j in range(i):
-			# print('j =', j, end=' ')
-			# print('price[j] =', price[j], end=' ')
-			# print('val[i-j-1] =', val[i-j-1], end=' ')
-			max_val = max(max_val, price[j] + val[i-j-1])
-		# print()
-		val[i] = max_val
-		# print('val =', val)
+    # Build the table val[] in bottom up manner and return
+    # the last entry from the table
+    # print('val =', val)
+    for i in range(1, n+1):
+        # print('i =', i)
+        max_val = INT_MIN
+        for j in range(i):
+            # print('j =', j, end=' ')
+            # print('price[j] =', price[j], end=' ')
+            # print('val[i-j-1] =', val[i-j-1], end=' ')
+            max_val = max(max_val, price[j] + val[i-j-1])
+        # print()
+        val[i] = max_val
+    # print('val =', val)
 
-	return val[n]
+    return val[n]
 
 arr = [x for x in p.values()]
 print(cutRod(arr, tam))
