54b86a7ad2452131251312c996e0d2d42b13ab54
antonio357 <carlosalbuquerque359@gmail.com>
2021-10-02 19:38:46 -0300

    questao1

--- Henrique/dynamic programming/questao1/iteractive_bottomUp_cutRod.py
+++ Henrique/dynamic programming/questao1/iteractive_bottomUp_cutRod.py
@@ -1,1 +1,17 @@
+from random import randint
 
+def iteractive_bottomUp_cutRod(prices, rod_len):
+    previous_prices, biggest_price = [0] * (rod_len + 1), 0
+    for rl in range(1, rod_len + 1):
+        for rl_slice in range(1, rl+1):
+            biggest_price = max(biggest_price, prices[rl_slice] + previous_prices[rl - rl_slice])
+        previous_prices[rl] = biggest_price
+    return biggest_price
+
+
+prices = dict()
+rod_len = 994
+for i in range(1, rod_len + 1):
+     prices[i] = randint(5, 10)
+
+print(iteractive_bottomUp_cutRod(prices, rod_len))
