c1a124344a733d180bfde074b7c348297da16efe
antonio357 <carlosalbuquerque359@gmail.com>
2021-10-03 18:05:57 -0300

    simplificando inicialização da matrix

--- Henrique/dynamic programming/questao2/bag.py
+++ Henrique/dynamic programming/questao2/bag.py
@@ -2,6 +2,12 @@ def printMatrix(m):
     print()
     for i in m: print(i)
 
+def initMatrix(m, n, w):
+    for r in range(w + 1):
+        m.append(list())
+        for c in range(n + 1):
+            m[r].append(0)
+
 def recursive():
     pass
 
@@ -10,19 +16,11 @@ def memoized():
 
 def dynamicPrograming(s, n, w):
     m = list()
-    for r in range(w + 1):
-        m.append(list())
-        for c in range(n + 1):
-            if r == 0 or c == 0: m[r].append(0)
-            else: m[r].append('-')
-    printMatrix(m)
-
+    initMatrix(m, n, w)
     for i in range(1, n + 1):
         for w in range(1, w + 1):
             if s[i].peso > w: m[w][i] = m[w][i - 1]
             else: m[w][i] = max(m[w - s[i].peso][i - 1] + s[i].valor, m[w][i - 1])
-
-    printMatrix(m)
     return m[w][n]
 
 class item():
