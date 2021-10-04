23e52f08b16277e26286341d937409e372a433c1
antonio357 <carlosalbuquerque359@gmail.com>
2021-10-04 14:29:25 -0300

    comparando a quantidade de recursões

--- Henrique/dynamic programming/questao2/bag.py
+++ Henrique/dynamic programming/questao2/bag.py
@@ -5,22 +5,21 @@ def recursive(s, n, w):
     elif s[n - 1].peso > w: return recursive(s, n - 1, w)
     else: return max(recursive(s, n - 1, w), s[n - 1].valor + recursive(s, n - 1, w - s[n - 1].peso))
 
-def aux_memoized(s, n, w):
+def aux_memoized(s, n, w, count=True, c=[0]):
     m = [[-1 for i in range(n + 2)] for j in range(w + 2)]
-    return memoized(s,n,w,m)
-c = 0
-def memoized(s, n, w, m):
-    global c
-    c += 1
+    return memoized(s,n,w,m, count, c)
+# c = 0
+def memoized(s, n, w, m, count=True, c=[0]):
+    c[0] += 1
     if n <= 1: return 0
     if m[w][n] != -1:
-        c -= 1
+        if count: c[0] -= 1
         return m[w][n]
     elif s[n - 1].peso > w:
-        m[w][n] = memoized(s, n - 1, w, m)
+        m[w][n] = memoized(s, n - 1, w, m, count, c)
         return m[w][n]
     else:
-        m[w][n] = max(memoized(s, n - 1, w, m), s[n - 1].valor + memoized(s, n - 1, w - s[n - 1].peso, m))
+        m[w][n] = max(memoized(s, n - 1, w, m, count, c), s[n - 1].valor + memoized(s, n - 1, w - s[n - 1].peso, m, count, c))
         return m[w][n]
 
 def printMatrix(m):
@@ -47,34 +46,6 @@ class item():
         self.valor = valor
         self.peso = peso
 
-def pac(s, w):
-    # driver code
-    val = [s[x].valor for x in s.keys()]
-    wt = [s[x].peso for x in s.keys()]
-    W = w
-    n = len(val)
-
-    # We initialize the matrix with -1 at first.
-    t = [[-1 for i in range(W + 1)] for j in range(n + 1)]
-    return knapsack(wt, val, W, n, t)
-def knapsack(wt, val, W, n, t):
-    # base conditions
-    if n == 0 or W == 0:
-        return 0
-    if t[n][W] != -1:
-        return t[n][W]
-
-    # choice diagram code
-    if wt[n - 1] <= W:
-        t[n][W] = max(
-            val[n - 1] + knapsack(
-                wt, val, W - wt[n - 1], n - 1, t),
-            knapsack(wt, val, W, n - 1, t))
-        return t[n][W]
-    elif wt[n - 1] > W:
-        t[n][W] = knapsack(wt, val, W, n - 1, t)
-        return t[n][W]
-
 for n in range(1, 63+1):
     s, sr, ws, vs, w = {}, {}, [], [], n
     for i in range(1, n+1):
@@ -85,10 +56,17 @@ for n in range(1, 63+1):
             ws.append(s[k].peso)
             vs.append(s[k].valor)
 
-    expected, found_r, found_m, test = dynamicPrograming(s, n, w), recursive(s, n + 1, w), aux_memoized(s, n + 1, w), pac(s, w)
+    c = [0]
+    found_m = aux_memoized(s, n + 1, w, True, c)
+    c1 = c[0]
+    c = [0]
+    test = aux_memoized(s, n + 1, w, False, c)
+    c2 = c[0]
+    print('m, !m, !m - m == {}, {}, {}'.format(c1, c2, c2 - c1))
+    expected, found_r = dynamicPrograming(s, n, w), recursive(s, n + 1, w)
     if not expected == found_r == found_m == test:
-        print('{} | {} | {} | {}'.format(expected, found_r, found_m, test))
+        # print('{} | {} | {} | {}'.format(expected, found_r, found_m, test))
         print('WRONG')
         brk = True
         break
-    print('{} == {} == {} == {}'.format(expected, found_r, found_m, test))
+    # print('{} == {} == {} == {}'.format(expected, found_r, found_m, test))
