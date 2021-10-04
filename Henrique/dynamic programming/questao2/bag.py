973f3faa3f731a746ab45f845fc4683c85db4b0b
antonio357 <carlosalbuquerque359@gmail.com>
2021-10-04 16:31:55 -0300

    implementação com dicionário ainda falha

--- Henrique/dynamic programming/questao2/bag.py
+++ Henrique/dynamic programming/questao2/bag.py
@@ -7,7 +7,9 @@ def recursive(s, n, w):
 
 def aux_memoized(s, n, w, count=True, c=[0]):
     m = [[-1 for i in range(n + 2)] for j in range(w + 2)]
-    return memoized(s,n,w,m, count, c)
+    r = memoized(s,n,w,m, count, c)
+    # printMatrix(m)
+    return r
 # c = 0
 def memoized(s, n, w, m, count=True, c=[0]):
     c[0] += 1
@@ -21,6 +23,20 @@ def memoized(s, n, w, m, count=True, c=[0]):
     else:
         m[w][n] = max(memoized(s, n - 1, w, m, count, c), s[n - 1].valor + memoized(s, n - 1, w - s[n - 1].peso, m, count, c))
         return m[w][n]
+
+def memoized_with_d(s, n, w, m=dict(), count=True, c=[0]):
+    c[0] += 1
+    if n <= 1: return 0
+    rid = '{},{}'.format(w,n)
+    if m.get(rid):
+        if count: c[0] -= 1
+        return m[rid]
+    elif s[n - 1].peso > w:
+        m[rid] = memoized_with_d(s, n - 1, w, m, count, c)
+        return m[rid]
+    else:
+        m[rid] = max(memoized_with_d(s, n - 1, w, m, count, c), s[n - 1].valor + memoized_with_d(s, n - 1, w - s[n - 1].peso, m, count, c))
+        return m[rid]
 
 def printMatrix(m):
     print()
@@ -62,11 +78,15 @@ for n in range(1, 63+1):
     c = [0]
     test = aux_memoized(s, n + 1, w, False, c)
     c2 = c[0]
-    print('m, !m, !m - m == {}, {}, {}'.format(c1, c2, c2 - c1))
+    c = [0]
+    d = dict()
+    test1 = memoized_with_d(s,n,w,d,True, c)
+    c3 = c[0]
+    # print('m, !m, !m - m, d == {}, {}, {}, {}'.format(c1, c2, c2 - c1, c3))
+    # print(d)
     expected, found_r = dynamicPrograming(s, n, w), recursive(s, n + 1, w)
-    if not expected == found_r == found_m == test:
-        # print('{} | {} | {} | {}'.format(expected, found_r, found_m, test))
+    if not expected == found_r == found_m == test == test1:
+        print('{} | {} | {} | {} | {}'.format(expected, found_r, found_m, test, test1))
         print('WRONG')
-        brk = True
-        break
-    # print('{} == {} == {} == {}'.format(expected, found_r, found_m, test))
+        # break
+    print('{} == {} == {} == {} == {}'.format(expected, found_r, found_m, test, test1))
