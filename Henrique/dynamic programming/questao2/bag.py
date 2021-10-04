348a1c60871b6ae011535c00fa21ac7e6b70c380
antonio357 <carlosalbuquerque359@gmail.com>
2021-10-04 01:04:23 -0300

    try 1 igual ao expected

--- Henrique/dynamic programming/questao2/bag.py
+++ Henrique/dynamic programming/questao2/bag.py
@@ -20,7 +20,7 @@ def initMatrix(m, n, w):
 # }
 
 def f(w, v, n, W, id=0, idP=-1, idc=None, idcP=None):
-    print('f({}, {}), id = {}, idP {}, idc = {}, idcP = {}'.format(n, W, id, idP, idc, idcP))
+    # print('f({}, {}), id = {}, idP {}, idc = {}, idcP = {}'.format(n, W, id, idP, idc, idcP))
     # print('f({}, {}, {}, {})'.format(w, v, n, W))
     # print(n, end=' ')
     if n <= 0:
@@ -36,10 +36,16 @@ def f(w, v, n, W, id=0, idP=-1, idc=None, idcP=None):
         x2 = v[n - 1] + f(w, v, n - 1, W - w[n - 1], id + 1, id, idc='///', idcP=idc)
         return max(x1, x2)
 
-# def recursive(s, n, w):
-#     if n == 0: return 0
-#     elif s[n - 1].peso > s[w].peso: return recursive(s, n - 1, w)
-#     return max(recursive(s, n - 1, w), s[n - 1].valor + recursive(s, n - 1, w - 1))
+def getFromdit(dit, key):
+    if dit.get(key):
+        return dit[k]
+    return item(-1, -1)
+
+def f_with_dict(items_value_weight, current_item, current_weight):
+    if current_item <= 0: return 0
+    elif getFromdit(items_value_weight, current_item - 1).peso > current_weight: return f_with_dict(items_value_weight, current_item - 1, current_weight)
+    else:
+        return max(f_with_dict(items_value_weight, current_item - 1, current_weight), getFromdit(items_value_weight, current_item).valor + f_with_dict(items_value_weight, current_item - 1, current_weight - getFromdit(items_value_weight, current_item - 1).peso))
 
 def memoized():
     pass
@@ -53,6 +59,13 @@ def dynamicPrograming(s, n, w):
             else: m[w][i] = max(m[w - s[i].peso][i - 1] + s[i].valor, m[w][i - 1])
     return m[w][n]
 
+def f1(v, w, n, W):
+    if W < 0: return -1000
+    if n < 0 or w == 0: return 0
+    c1 = v[n] + f1(v, w, n - 1, W - w[n])
+    c2 = f1(v, w, n - 1, W)
+    return max(c1, c2)
+
 class item():
     def __init__(self, peso, valor):
         self.valor = valor
@@ -64,14 +77,16 @@ s = {
     3: item(4, 16),
     4: item(2, 9)
 }
-n, w = 4, 6
+n, w = 4, 10
 
-print(dynamicPrograming(s, n, w))
+print('expected', dynamicPrograming(s, n, w))
 # print(recursive(s, n, w))
 w1, v1, n1, W1 = [0], [0], n, w
 for k in s.keys():
     w1.append(s[k].peso)
     v1.append(s[k].valor)
-print('f(n, W)')
-print('f({}, {})'.format(n1, W1))
-print(f(w1, v1, n1, W1))
+# print('f(n, W)')
+# print('f({}, {})'.format(n1, W1))
+print('try 1', f(w1[1:], v1[1:], n1, W1))
+# print('try 2', f1(w1[1:], v1[1:], n1, W1))
+print('my try 1', f_with_dict(s, n, w))
