1e113be324ca2ec17488fad98f809c67a38f6a5e
antonio357 <carlosalbuquerque359@gmail.com>
2021-10-04 01:13:22 -0300

    removendo tentativa nada a ver

--- Henrique/dynamic programming/questao2/bag.py
+++ Henrique/dynamic programming/questao2/bag.py
@@ -8,17 +8,6 @@ def initMatrix(m, n, w):
         for c in range(n + 1):
             m[r].append(0)
 
-# int knapsackRec(int[] w, int[] v, int n, int W) {
-#     if (n <= 0) {
-#         return 0;
-#     } else if (w[n - 1] > W) {
-#         return knapsackRec(w, v, n - 1, W);
-#     } else {
-#         return Math.max(knapsackRec(w, v, n - 1, W), v[n - 1]
-#           + knapsackRec(w, v, n - 1, W - w[n - 1]));
-#     }
-# }
-
 def f(w, v, n, W, id=0, idP=-1, idc=None, idcP=None):
     # print('f({}, {}), id = {}, idP {}, idc = {}, idcP = {}'.format(n, W, id, idP, idc, idcP))
     # print('f({}, {}, {}, {})'.format(w, v, n, W))
@@ -28,12 +17,12 @@ def f(w, v, n, W, id=0, idP=-1, idc=None, idcP=None):
         return 0
     elif w[n - 1] > W:
         # print('f({}, {}), elif'.format(n - 1, W))
-        return f(w, v, n - 1, W, id + 1, id, idc='/', idcP=idc)
+        return f(w, v, n - 1, W, id + 1, id, '/', idc)
     else:
         # print('f({}, {}), else, first'.format(n - 1, W))
-        x1 = f(w, v, n - 1, W, id + 1, id, idc='//', idcP=idc)
+        x1 = f(w, v, n - 1, W, id + 1, id, '//', idc)
         # print('f({}, {}), else, then'.format(n - 1, W - w[n - 1]))
-        x2 = v[n - 1] + f(w, v, n - 1, W - w[n - 1], id + 1, id, idc='///', idcP=idc)
+        x2 = v[n - 1] + f(w, v, n - 1, W - w[n - 1], id + 1, id, '///', idc)
         return max(x1, x2)
 
 def getFromdit(dit, key):
@@ -59,13 +48,6 @@ def dynamicPrograming(s, n, w):
             else: m[w][i] = max(m[w - s[i].peso][i - 1] + s[i].valor, m[w][i - 1])
     return m[w][n]
 
-def f1(v, w, n, W):
-    if W < 0: return -1000
-    if n < 0 or w == 0: return 0
-    c1 = v[n] + f1(v, w, n - 1, W - w[n])
-    c2 = f1(v, w, n - 1, W)
-    return max(c1, c2)
-
 class item():
     def __init__(self, peso, valor):
         self.valor = valor
@@ -81,12 +63,11 @@ n, w = 4, 10
 
 print('expected', dynamicPrograming(s, n, w))
 # print(recursive(s, n, w))
-w1, v1, n1, W1 = [0], [0], n, w
+w1, v1, n1, W1 = [], [], n, w
 for k in s.keys():
     w1.append(s[k].peso)
     v1.append(s[k].valor)
 # print('f(n, W)')
 # print('f({}, {})'.format(n1, W1))
-print('try 1', f(w1[1:], v1[1:], n1, W1))
-# print('try 2', f1(w1[1:], v1[1:], n1, W1))
-print('my try 1', f_with_dict(s, n, w))
+print('try', f(w1, v1, n1, W1))
+print('my try', f_with_dict(s, n, w))
