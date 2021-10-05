13dea4bd031d53f7f0e2e7cf9e31e2ba43676fb6
antonio357 <carlosalbuquerque359@gmail.com>
2021-10-04 23:44:07 -0300

    done

--- Henrique/dynamic programming/questao3/lcs.py
+++ Henrique/dynamic programming/questao3/lcs.py
@@ -5,7 +5,11 @@ def recursive(X, Y, x, y):
 
 
 def memoized(X, Y, m, n):
-    C = [[None] * (n + 1) for i in range(m + 1)]
+    C = []
+    for x in range(m + 1):
+        C.append([])
+        for y in range(n + 1):
+            C[x].append(None)
     return memoized_recursion(X, Y, C, len(X), len(Y))
 
 def memoized_recursion(X, Y, C, x, y):
@@ -17,19 +21,19 @@ def memoized_recursion(X, Y, C, x, y):
 
 
 def dynamicPrograming(X, Y, m, n):
-    C = [[None] * (n + 1) for i in range(m + 1)]
+    C = []
+    for x in range(m + 1):
+        C.append([])
+        for y in range(n + 1):
+            C[x].append(-1)
     for x in range(m + 1): C[x][0] = 0
     for y in range(n + 1): C[0][y] = 0
 
     for x in range(m + 1):
         for j in range(n + 1):
-            if x == 0 or y == 0:
-                C[x][y] = 0
-            elif X[x - 1] == Y[y - 1]:
-                C[x][y] = C[x - 1][y - 1] + 1
-            else:
-                C[x][y] = max(C[x - 1][y], C[x][y - 1])
-
+            if x == 0 or y == 0: C[x][y] = 0
+            elif X[x - 1] == Y[y - 1]: C[x][y] = C[x - 1][y - 1] + 1
+            else: C[x][y] = max(C[x - 1][y], C[x][y - 1])
     return C[m][n]
 
 test_cases = [
@@ -41,5 +45,5 @@ test_cases = [
 for t in test_cases:
     X, Y = t[0], t[1]
     m, n = len(X), len(Y)
-    r, m = recursive(X, Y, m, n), memoized(X, Y, m, n)
-    print(r, m)
+    r, m, pd = recursive(X, Y, m, n), memoized(X, Y, m, n), dynamicPrograming(X, Y, m, n)
+    print(r, m, pd)
